56c4da466dc6afd45a7d5fbb5dca7d22d3be18f8
Ryoki Tanaka <ry_tanaka@yumemi.co.jp>
2022-10-05 21:54:49 +0900

    fix: UserモデルをDBに合わせる

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,15 +2,12 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
-use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    use HasFactory;
 
     /**
      * The attributes that are mass assignable.
@@ -21,6 +18,7 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'first_logged_in_at'
     ];
 
     /**
@@ -30,7 +28,6 @@ class User extends Authenticatable
      */
     protected $hidden = [
         'password',
-        'remember_token',
     ];
 
     /**
@@ -39,6 +36,8 @@ class User extends Authenticatable
      * @var array<string, string>
      */
     protected $casts = [
-        'email_verified_at' => 'datetime',
+        'first_logged_in_at' => 'datetime:Y-m-d H:i:s.v',
+        'created_at' => 'datetime:Y-m-d H:i:s.v',
+        'updated_at' => 'datetime:Y-m-d H:i:s.v',
     ];
 }
