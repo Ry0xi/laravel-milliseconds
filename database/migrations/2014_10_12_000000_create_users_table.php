51b7753a574b93823ecb06583a1ed0ef48d7a27a
Ryoki Tanaka <ry_tanaka@yumemi.co.jp>
2022-10-05 21:50:34 +0900

    fix: precisionを設定

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,10 +17,10 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
+            // precisionを指定すると、保存する時間の精度を指定できる
+            $table->timestamps(3);
+            $table->dateTime('first_logged_in_at', 3);
         });
     }
 
